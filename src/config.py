a1172f6ac23f44bb60dd78e079c5245a725c885b
Sujit Ahirrao <sujitahirrao3@gmail.com>
2020-08-26 13:58:17 +0530

    Update API router with ENV

--- src/config.py
+++ src/config.py
@@ -1,6 +1,6 @@
 import os
 
-env = "dev"     # [dev/test/prod]
+ENV = "dev"     # [dev/test/prod]
 
 PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
 print("PROJECT_DIR:\t", PROJECT_DIR)
@@ -9,7 +9,7 @@ DATA_FOLDER = os.path.join(PROJECT_DIR, 'data')
 MODELS_FOLDER = os.path.join(PROJECT_DIR, 'models')
 LOGS_FOLDER = os.path.join(PROJECT_DIR, 'logs')
 
-if env == "dev":
+if ENV == "dev":
     print("DATA_FOLDER:\t", DATA_FOLDER)
     print("MODELS_FOLDER:\t", MODELS_FOLDER)
     print("LOGS_FOLDER:\t", LOGS_FOLDER)
